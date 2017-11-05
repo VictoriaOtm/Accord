e6568bb481fe717cb2961ad4349c7f1a36e2fe31
Leonid <leonid.sobolev285@gmail.com>
2017-11-06 01:02:36 +0300

    Implemented basic player slots

--- player.cpp
+++ player.cpp
@@ -1,6 +1,30 @@
 #include "player.h"
 
-Player::Player()
-{
+static Player& Player::Instance(){
+    static Player p;
+    return p;
+}
 
+void Player::Play(){
+    player.play();
+}
+
+void Player::Pause(){
+    player.pause();
+}
+
+void Player::Stop(){
+    player.stop();
+}
+
+void Player::Prev(){
+    player.media().playlist()->previous();
+}
+
+void Player::Next(){
+    player.media().playlist()->next();
+}
+
+void Player::SetVolume(int volume){
+    player.setVolume(volume);
 }
