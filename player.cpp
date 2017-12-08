96fe14777febecd8bf7cef9ec6342aa3a8b48b9c
Leonid <leonid.sobolev285@gmail.com>
2017-12-08 11:36:42 +0300

    Слот addTracks: добавлена проверка MIME типа входного аудиофайла

--- player.cpp
+++ player.cpp
@@ -62,18 +62,32 @@ void Player::setSelectedAudioPosition(int position){
 }
 
 void Player::addTracks(const QVector<Audio>& newTracks){
-    QList<QMediaContent> tracks;
-    for(auto i = newTracks.begin() ; i != newTracks.end() ; ++i){
-        QMediaContent track(QMediaResource( QUrl::fromLocalFile(i->GetPath()) )); //create a QT media object by track path
-        tracks.append(track);
+    QList<QMediaContent> tracks;  //готовим массив для добавления в плейлист
+    QList<QString> errors; //массив для возврата непринятых файлов в интерфейс
+    QMimeDatabase db;       //инициализируем базу данных MIME типов для проверки типа файла
+    for(Audio& audio: newTracks){
+        QMimeType type = db.mimeTypeForFile(audio.GetPath(), QMimeDatabase::MatchContent); //получаем MIME тип файла на основе его контента
+        if(SUPPORTED_FORMATS.contains(type)){ //если такой тип поддерживается
+            QMediaContent track(QUrl::fromLocalFile(audio.GetPath())); //то конструируем MediaContent на его основе
+            qDebug() << "Adding track " << audio.GetPath();
+            qDebug() << type.name();
+            qDebug() << type.parentMimeTypes();
+            tracks.append(track); //добавляем к трекам, которые пойдут в плейлист
+        }else{
+            //добавляем имя файла к массиву ошибок
+            errors.append(audio.GetPath());
+        }
     }
     if(!player.playlist()->addMedia(tracks)){
         std::cerr << "Error while adding media to playlist in player" << std::endl;
         std::cerr << player.playlist()->errorString().toStdString() << std::endl;
         emit addTracksFailed();
     }else{
-        emit addedTracksSuccessfully();
+        if(!errors.empty()){
+            //подать сигнал о непринятых файлах
+        }
     }
+
 }
 
 void Player::removeTracks(int start, int end){
