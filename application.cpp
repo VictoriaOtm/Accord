319c4b3653b9a28c034564af73f58f8ca24725dc
Leonid <leonid.sobolev285@gmail.com>
2017-12-08 11:35:16 +0300

    Убрано соединение диалога добавления треков с интерфейсом

--- application.cpp
+++ application.cpp
@@ -16,11 +16,14 @@ int Application::run(int argc, char *argv[]){
     // сначала скормить новый список песен в плеер
     // проверить его на валидность, а потом уже
     // показывать в интерфейсе юзеру
-    QObject::connect(&uploadWinController, SIGNAL(TracksAdded(QVector<Audio>)),
+    /*QObject::connect(&uploadWinController, SIGNAL(TracksAdded(QVector<Audio>)),
                           &mainController, SLOT(NewTracksAdded(QVector<Audio>)));
 
     QObject::connect(&uploadWinController, SIGNAL(TracksAdded(QVector<Audio>)),
-                        &Player::instance(), SLOT(addTracks(QVector<Audio>)));
+                        &Player::instance(), SLOT(addTracks(QVector<Audio>)));*/
+
+    QObject::connect(&uploadWinController, SIGNAL(TracksAdded(QVector<Audio>)),
+                     &Player::instance(), SLOT(addTracks(QVector<Audio>)));
 
     QObject::connect(&mainController.getMainWin(), SIGNAL(play(bool)),
                      &Player::instance(), SLOT(play(bool)));
